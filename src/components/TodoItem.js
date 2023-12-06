4d617fa154502d68256078c6275c640f974f6697
ravishkaw <127718357+ravishkaw@users.noreply.github.com>
2023-12-06 11:23:40 +0530

    update todo app

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,21 +1,14 @@
-import { useState } from "react";
-
-function TodoItem(props) {
-  const [isClicked, setClick] = useState(false);
-
-  function listItemClick() {
-    setClick((prevValue) => {
-      return !prevValue;
-    });
+function ToDoItem(props) {
+    return (
+      <div
+        onClick={() => {
+          props.onChecked(props.id);
+        }}
+      >
+        <li>{props.text}</li>
+      </div>
+    );
   }
-  return (
-    <li
-      style={{ textDecoration: isClicked ? "line-through" : "none" }}
-      onClick={listItemClick}
-    >
-      {props.text}
-    </li>
-  );
-}
-
-export default TodoItem;
+  
+  export default ToDoItem;
+  
